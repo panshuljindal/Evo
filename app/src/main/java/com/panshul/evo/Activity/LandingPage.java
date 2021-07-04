86ecd5e83084d9b5e75dd07f56c6213de9217d05
panshuljindal <60618820+panshuljindal@users.noreply.github.com>
2021-07-04 17:24:18 +0530

    Popular UI

---
+++ app/src/main/java/com/panshul/evo/Activity/LandingPage.java
@@ -0,0 +1,16 @@
+package com.panshul.evo.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.panshul.evo.R;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
